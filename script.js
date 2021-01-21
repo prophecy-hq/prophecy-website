0b501b16e885e84f787cdca7f45f4c0895438cd1
Siddharth Ahuja <siddharth@Siddharths-MBP.Dlink>
2021-01-21 19:24:53 +0530

    made animations css only

--- script.js
+++ script.js
@@ -12,13 +12,3 @@ function(){
     animation.goToAndPlay(0)
     animation.play();
 })
-
-
-window.onload = function() {
-
-    document.getElementById("animheader").after.className += " slidein";
-    document.getElementById("animleft").className += " fadein";
-    document.getElementById("animright").className += " fadein";
-
-    
-}
