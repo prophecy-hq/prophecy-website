280dabbbd2fecf53b57317e1547f23b113ab0cc0
Siddharth Ahuja <siddharth@Siddharths-MBP.Dlink>
2021-01-21 20:45:00 +0530

    implemented the copy to clipboard action

--- script.js
+++ script.js
@@ -11,4 +11,34 @@ document.getElementById('anim').addEventListener('click',
 function(){ 
     animation.goToAndPlay(0)
     animation.play();
+
+    copyToClipBoard("hello@prophecy.one");
+    
+    setTimeout(function() {
+        document.getElementById("copied-to-clipboard-container").className +=  " showCopiedText";
+    }, 1000);
+
+    setTimeout(function() {
+        document.getElementById("copied-to-clipboard-container").className +=  " opacity1";
+    }, 1400);
+    
+    setTimeout(function() {
+        document.getElementById("copied-to-clipboard-container").classList.remove("showCopiedText");
+        document.getElementById("copied-to-clipboard-container").className +=  " hideCopiedText";
+        document.getElementById("copied-to-clipboard-container").classList.remove("opacity1");
+    }, 3400);
+
+        document.getElementById("copied-to-clipboard-container").classList.remove("hideCopiedText");
 })
+
+
+
+const copyToClipBoard = (str) =>
+{
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
