86a0b6c34b68245c5e7c456fd914ac5553980cbe
Siddharth Ahuja <siddharth@Siddharths-MBP.Dlink>
2021-01-21 18:22:43 +0530

    added animations and transitions

--- script.js
+++ script.js
@@ -12,3 +12,13 @@ function(){
     animation.goToAndPlay(0)
     animation.play();
 })
+
+
+window.onload = function() {
+
+    document.getElementById("animheader").after.className += " slidein";
+    document.getElementById("animleft").className += " fadein";
+    document.getElementById("animright").className += " fadein";
+
+    
+}
