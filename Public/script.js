161d4d58e0d98d2b525abff7f402932840b55a70
Siddharth Ahuja <siddharth@Siddharths-MBP.Dlink>
2021-01-22 14:24:31 +0530

    implemented email signup js

--- Public/script.js
+++ Public/script.js
@@ -1,3 +1,5 @@
+
+//Lottie animation properties
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('anim'),
     rendered: 'svg',
@@ -6,7 +8,7 @@ var animation = bodymovin.loadAnimation({
     path: 'data.json'
 })
 
-
+//Lottie animation + Copy to Clipboard animation
 document.getElementById('anim').addEventListener('click', 
 function(){ 
     animation.goToAndPlay(0)
@@ -32,7 +34,7 @@ function(){
 })
 
 
-
+//Copy to clipboard function
 const copyToClipBoard = (str) =>
 {
     const el = document.createElement('textarea');
@@ -42,3 +44,90 @@ const copyToClipBoard = (str) =>
     document.execCommand('copy');
     document.body.removeChild(el);
 };
+
+
+
+//Email Signup Form//
+var notificationTimer;	//Global timer variable required for clearTimeout()//
+$(function () {
+    var $form = $('#mc-embedded-subscribe-form');
+    $('#mc-embedded-subscribe').on('click', function(event) {
+        if(event) {
+            event.preventDefault();
+            }
+        $('#notification_container').clearQueue().fadeOut(200);
+        $('#notification_container').html('<span></span>');
+        register($form);
+        $(this).fadeOut(250);
+        $('#loadingAnimation').delay(250).fadeIn(100);
+        });
+    });
+    
+function register($form) {
+    $.ajax({
+        type: $form.attr('method'),
+        url: $form.attr('action'),
+        data: $form.serialize(),
+        cache       : false,
+        dataType    : 'json',
+        contentType: "application/json; charset=utf-8",
+        timeout: 10000,
+        error       : function() {
+            $('#notification_container').html('<span class="alert"> Could not connect to server. Please try again later.</span>');
+            $('#notification_container').fadeIn(500);
+            notificationTimer = setTimeout(function() {
+                if ($('#notification_container .alert').css('opacity') === '1') {
+                    $('#notification_container .alert').fadeToggle(2000);}
+                }, 7000);
+            $('#notification_container').hover(function(){ //Prevent notification from fading out during hover//
+                clearTimeout(notificationTimer);
+                $('#notification_container .alert').filter(':animated').stop(true, false).fadeToggle(200);
+                },
+                function(){
+                    notificationTimer = setTimeout(function() {
+                        if ($('#notification_container .alert').css('opacity') === '1') {
+                            $('#notification_container .alert').fadeToggle(2000);}
+                    }, 7000);
+                });
+            $('#mc-embedded-subscribe').filter(':not(:animated)').delay(500).fadeIn(250); //Bring back the submit button//
+            },
+                
+        success     : function(data) {
+            var message;
+            if (data.result !== "success") {
+                message = data.msg.substring(4);
+                $('#notification_container').html('<span class="alert">'+' Error: '+message+'</span>');
+                $('#formContainer').filter(':not(:animated)').effect('shake', {distance: 3});
+                $('#mc-embedded-subscribe').delay(500).fadeIn(250);
+                $('#notification_container').fadeIn(500); //Fade in the notification//
+                notificationTimer = setTimeout(function() {
+                    if ($('#notification_container .alert').css('opacity') === '1') {
+                        $('#notification_container .alert').fadeToggle(2000);}
+                    }, 7000);
+                $('#notification_container').hover(function(){ //Prevent notification from fading out during hover//
+                    clearTimeout(notificationTimer);
+                    $('#notification_container .alert').filter(':animated').stop(true, false).fadeToggle(200);
+                    /*if ($('#notification_container .alert').css('opacity') < 1) {	//Trigger fadeToggle only is animation is already fading//
+                        $('#notification_container .alert').stop(true, false).fadeToggle(200);
+                        }*/
+                    },
+                    function(){
+                        notificationTimer = setTimeout(function() {
+                            if ($('#notification_container .alert').css('opacity') === '1') {
+                                $('#notification_container .alert').fadeToggle(2000);}
+                            }, 7000);
+                    });
+                }
+            else {
+                message = 'Done! Check your mailbox and verify.';
+                $('#notification_container').html('<span class="success">'+message+'</span>');
+                clearTimeout(notificationTimer);
+                $('#notification_container').fadeIn(500);
+                $('#mce-EMAIL').blur();
+                }
+            },
+        complete : function(){
+            $('#loadingAnimation').fadeOut(500);
+            }
+        });
+    }
