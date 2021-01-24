d01dc7ce014961ad6633ac5561b458402e6c42bd
Akshay Verma <akshayverma1@users.noreply.github.com>
2021-01-24 16:45:50 +0530

    Site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-		author: `@gatsbyjs`,
+		title: `Gatsby Test`,
+		description: `Learning to use Gatsby.`,
+		url: 'https://prophecy.one',
+		author: `Akshay Verma`,
+		twitterUsername: '@prophecy_hq',
 	},
 	plugins: [
 		`gatsby-plugin-react-helmet`,
