bff7d8b3237f3df7d99e306d3d9b32f1f10c3110
Akshay Verma <akshayverma1@users.noreply.github.com>
2021-01-24 16:12:27 +0530

    Fixed ESLint and Prettier

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,4 @@
-import { action } from '@storybook/addon-actions'
-import { addDecorator } from '@storybook/react'
-/** @jsx jsx */
-import { jsx, ThemeProvider } from 'theme-ui'
-
+import { action } from '@storybook/addon-actions';
 // Gatsby's Link overrides:
 // Gatsby Link calls the `enqueue` & `hovering` methods on the global variable ___loader.
 // This global object isn't set in storybook context, requiring you to override it to empty functions (no-op),
@@ -10,11 +6,11 @@ import { jsx, ThemeProvider } from 'theme-ui'
 global.___loader = {
 	enqueue: () => {},
 	hovering: () => {},
-}
-// This global variable is prevents the '__BASE_PATH__ is not defined' error inside Storybook.
-global.__BASE_PATH__ = '/'
+};
+// This global variable is prevents the "__BASE_PATH__ is not defined" error inside Storybook.
+global.__BASE_PATH__ = '/';
 // Navigating through a gatsby app using gatsby-link or any other gatsby component will use the `___navigate` method.
 // In Storybook it makes more sense to log an action than doing an actual navigate. Checkout the actions addon docs for more info: https://github.com/storybookjs/storybook/tree/master/addons/actions.
-window.___navigate = pathname => {
-	action('NavigateTo:')(pathname)
-}
+window.___navigate = (pathname) => {
+	action('NavigateTo:')(pathname);
+};
--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,9 @@ module.exports = {
 	},
 	plugins: [
 		`gatsby-plugin-react-helmet`,
+		`gatsby-plugin-theme-ui`,
+		`gatsby-plugin-sharp`,
+		`gatsby-transformer-sharp`,
 		{
 			resolve: `gatsby-source-filesystem`,
 			options: {
@@ -13,8 +16,6 @@ module.exports = {
 				path: `${__dirname}/src/images`,
 			},
 		},
-		`gatsby-transformer-sharp`,
-		`gatsby-plugin-sharp`,
 		{
 			resolve: `gatsby-plugin-manifest`,
 			options: {
@@ -27,10 +28,8 @@ module.exports = {
 				icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
 			},
 		},
-		`gatsby-plugin-eslint`,
-		`gatsby-plugin-theme-ui`,
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.dev/offline
 		// `gatsby-plugin-offline`,
 	],
-}
+};
